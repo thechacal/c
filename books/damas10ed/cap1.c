bf78a9be0df525c4887461a4b7133d707f7bd885
ThEcHaCaL <edluisecosta@gmail.com>
2015-09-18 00:18:16 -0300

    Commit by github.h

--- books/damas10ed/cap1.c
+++ books/damas10ed/cap1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "github.h"
+#include "libs/github.h"
 
 int main(int argc, char const *argv[]) {
   push();
---
+++ books/damas10ed/libs/github.h
@@ -0,0 +1,5 @@
+void push() {
+  system("git add *");
+  system("git commit -m 'Commit by github.h'");
+  system("git push origin master");
+}
