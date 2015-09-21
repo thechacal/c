788da404dd90134ed44cb6f6bc25168ae9eb07ae
ThEcHaCaL <edluisecosta@gmail.com>
2015-09-21 15:27:00 -0300

    Commited by github.h

--- mylibs/github.h
+++ mylibs/github.h
@@ -1,5 +1,5 @@
 void push() {
-  system("git add ../Exercises\ from\ Books/*.c ../mylibs/*");
+  system("git add ../*.c ../mylibs/*");
   system("git commit -m 'Commited by github.h'");
   system("git push origin master");
 }
