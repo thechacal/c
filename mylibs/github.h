a7a21ffb40a9b8b75a6f1627a5be6e656b9c6e61
ThEcHaCaL <edluisecosta@gmail.com>
2015-09-21 15:11:14 -0300

    My Libs

--- mylibs/github.h
+++ mylibs/github.h
@@ -1,5 +1,5 @@
 void push() {
-  system("git commit -a *.c mylibs/* -m 'Commited by github.h'");
+  system("git commit *.c mylibs/* -m 'Commited by github.h'");
   //system("git commit -m 'Commited by github.h'");
   system("git push origin master");
 }
