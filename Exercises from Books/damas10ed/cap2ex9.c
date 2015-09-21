81c0c15329263a58385a22b074e88d4822c2b8d6
ThEcHaCaL <edluisecosta@gmail.com>
2015-09-21 17:06:13 -0300

    Commited by github.h

--- Exercises from Books/damas10ed/cap2ex9.c
+++ Exercises from Books/damas10ed/cap2ex9.c
@@ -17,4 +17,3 @@ int main(int argc, char const *argv[]) {
 
   return 0;
 }
-//lol
--- mylibs/github.h
+++ mylibs/github.h
@@ -1,5 +1,4 @@
 void push() {
-  //system("ls *.c ../../mylibs/*.h");
   system("git add *.c ../../mylibs/*.h");
   system("git commit -m 'Commited by github.h'");
   system("git push origin master");
