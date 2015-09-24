c0688cfcc4eb568b53ca2beef7211ae6f280b6aa
ThEcHaCaL <edluisecosta@gmail.com>
2015-09-23 22:25:03 -0300

    Commited by github.h

--- Exercises from Books/damas10ed/cap2ex10.c
+++ Exercises from Books/damas10ed/cap2ex10.c
@@ -4,13 +4,11 @@
 
 int main(int argc, char const *argv[]) {
 
-  char str[10],ano[10],mes[10],dia[10];
-  int i=0;
-  int j=0;
-  int pos[1];
-  int tamanho;
+  char str[10],mes[10],dia[10];
+  int i,j,pos[1];
 
-  printf("Date at the format aaaa-mm-dd:");
+  i=j=0;
+  printf("\nType de Date at the format aaaa-mm-dd: ");
   scanf(" %10[^\n]",str);
   while(str[i]!='\0'){
     if(str[i]=='-'){
@@ -24,23 +22,19 @@ int main(int argc, char const *argv[]) {
     dia[j]=str[i];
     j+=1;
   }
-  //printf("%s\n",dia);
   j=0;
   for(i=pos[0]+1;i<pos[1];i++){
     mes[j]=str[i];
     j+=1;
   }
-  //printf("%s\n",mes);
   j=0;
-  //printf("%d\n",pos[0]);
   printf("New format: %s/%s/",dia,mes);
   for(i=0;i<pos[0];i++){
-    ano[j]=str[i];
     printf("%c",str[i]);
     j+=1;
   }
-  push("\n");
-  //push();
+  puts("\n");
+  push();
 
   return 0;
 }
