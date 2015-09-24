33f587cecb0e76d16ffd3aef2c3498ed51746ffc
ThEcHaCaL <edluisecosta@gmail.com>
2015-09-23 22:03:25 -0300

    Commited by github.h

--- Exercises from Books/damas10ed/cap2ex10.c
+++ Exercises from Books/damas10ed/cap2ex10.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 #include "../../mylibs/github.h" // https://github.com/thechacal/c/tree/master/mylibs
 
 int main(int argc, char const *argv[]) {
 
-  char str[15];
+  char str[10],ano[10],mes[10],dia[10];
+  int i=0;
+  int j=0;
+  int pos[1];
+  int tamanho;
 
   printf("Date at the format aaaa-mm-dd:");
-  scanf("%s",str);
-  printf("%s\n",str);
-
-  push();
+  scanf(" %10[^\n]",str);
+  while(str[i]!='\0'){
+    if(str[i]=='-'){
+      pos[j]=i;
+      j+=1;
+    }
+    i+=1;
+  }
+  j=0;
+  for(i=(pos[1])+1;i<strlen(str);i++){
+    dia[j]=str[i];
+    j+=1;
+  }
+  //printf("%s\n",dia);
+  j=0;
+  for(i=pos[0]+1;i<pos[1];i++){
+    mes[j]=str[i];
+    j+=1;
+  }
+  //printf("%s\n",mes);
+  j=0;
+  //printf("%d\n",pos[0]);
+  printf("New format: %s/%s/",dia,mes);
+  for(i=0;i<pos[0];i++){
+    ano[j]=str[i];
+    printf("%c",str[i]);
+    j+=1;
+  }
+  push("\n");
+  //push();
 
   return 0;
 }
